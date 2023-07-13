01e8f563159212e44ba7e679d503da6012421ac3
jbruce0308 <jessebruce035@gmail.com>
2023-07-13 17:11:56 -0400

    render apps in 2 columns

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
+import pandas
 st.set_page_config(layout="wide")
-col1, col2 = st.columns(2)
-
+col1, col2, = st.columns(2)
+col3, col4 = st.columns(2)
 
 
 with col1:
@@ -14,5 +15,19 @@ with col2:
     in the cloud space for 3 years
     """
     st.info(content)
-content2 = """Below you can find some of the apps i have built in Python. Feel free to contact me soon"""
+
+content2 = """Below you can find some of the apps i have built in Python. Feel free to contact me soon!"""
 st.write(content2)
+
+
+df = pandas.read_csv("data.csv",sep=";")
+
+
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
